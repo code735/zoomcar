d6d939cda36a0ac4d7b7309b568bc8c9f680642c
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-02-08 14:40:26 +0530

    Slider done but without login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Navbar from "./Components/Navbar";
 import "./App.css";
-import Hero from "./Components/Hero";
 import AllRoutes from "./Components/Routes/AllRoutes";
+import { useState, useEffect } from "react";
+import Slider from "./Components/Slider";
 
 function App() {
+  const [menu, setmenu] = useState(false);
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
+      <Slider setmenu={setmenu} menu={menu} />
+      <Navbar setmenu={setmenu} menu={menu} />
       <AllRoutes />
     </div>
   );

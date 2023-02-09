654fcf369718f9d6b845bead881a8772fd24883e
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-02-09 14:16:00 +0530

    location functionality added

--- src/Components/FIndcars.jsx
+++ src/Components/FIndcars.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function FIndcars() {
@@ -17,19 +17,58 @@ export default function FIndcars() {
         }
     }
 
+        const [location, setLocation] = useState({});
+
+        const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setLocation({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                });
+            },
+            (error) => console.error(error)
+            );
+        } else {
+            console.error("Geolocation is not supported by this browser.");
+            alert("Geolocation is not supported by this browser.");
+        }
+        };
+
+        useEffect(() => {
+            getLocation();
+          }, []);
+
+          useEffect(() => {
+            if (!location.lat || !location.lng) {
+              return;
+            }
+        
+            const API_ENDPOINT = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${location.lat}&lon=${location.lng}`;
+        
+            fetch(API_ENDPOINT)
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data);
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          }, [location]);
+
   return (
     <div style={{
         display:"flex",
         flexDirection:"column",
         alignItems:"center",
-        margin:"20px 0"
+        margin:"20px 0",
+        fontSize:"14px"
     }}>
         <div style={{
             display:"flex",
             justifyContent:"center",
-            fontSize:"14px" ,
-            textAlign:'center',
-            width:"100%"  
+            textAlign:'center'
         }}
         className="trip"
         >
@@ -48,10 +87,24 @@ export default function FIndcars() {
         </div>
 
         {/* Trip Location */}
-        
+        <div className="trip_location" onClick={getLocation} style={{display:"flex",alignItems:"center",background:"white",width:"100%",borderRadius:"5px"}}>
+            <i class="bi bi-dot" style={{fontSize:"2rem",color:"#10a310"}}></i>
+            <p>
+            Mumbai
+            <i class="bi bi-dot" style={{fontSize:"1rem",color:"#10a310"}}></i>   
+            226Q+3PM, Diwale Village, Sector 14, CBD Belapur, Navi Mumbai, Maharashtra 400614, India</p>
+            {
+                console.log(location)
+            }
+        </div>
 
-        <Link to="/products">
-            <button>Find Cars</button>
+        {/* trip dates */}
+        <div className="trip_date">
+
+        </div>
+            
+        <Link to="/products" className='find_cars_btn'>
+            <button style={{width:"100%",padding:"14px",border:'none',borderRadius:"5px"}}>Find Cars</button>
         </Link>
     </div>
   )

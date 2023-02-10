6de79a23d7fb72bfd21d6f11e3938e376a8400f0
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-02-10 23:01:10 +0530

    carpusel addded

--- src/Components/RangeCars.jsx
+++ src/Components/RangeCars.jsx
@@ -1,37 +1,15 @@
+import Carousel from './Carousel';
 import React from 'react'
-import carsData from '../cars_db.json';
 import { useState,useEffect } from 'react';
 
 export default function RangeCars() {
-
-let [productData, setProductData] = useState(carsData);
-
-  console.log(productData);
-
-  let cars=[];
-
-  for(let i=0;i<10;i++){
-    cars.push(
-        <div key={i} style={{
-            width:"20%"
-        }}>
-            <h6>{productData[i].car_data.name}</h6>
-            <img src={productData[i].car_data.url} alt="" />
-        </div>
-    )
-  }
-
   return (
     <div style={{
         textAlign:"center",
         color:"black"
     }}>
        <h3>CHOOSE FROM OUR WIDE RANGE OF CARS</h3>
-       <div style={{display:"flex",alignItems:"center"}}>
-        {
-            cars
-        }
-       </div>
+       <Carousel/>
     </div>
   )
 }

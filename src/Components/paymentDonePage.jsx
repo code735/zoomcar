a9453a72303a852442db90e9e993890f2db0b945
yash0922 <yashkumarpal987@gmail.com>
2023-02-11 15:41:03 +0530

    PaymentPageFuctioningAndUIdone

---
+++ src/Components/paymentDonePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function paymentDonePage(props) {
+    return (
+        <div>
+            <h1>Payment Successful</h1>
+            <img src="https://tenor.com/bypC1.gif" alt="" />
+        </div>
+    );
+}
+
+export default paymentDonePage;

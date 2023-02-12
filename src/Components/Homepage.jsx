3c0a642ddf624fc200a1456e481a205cd71d93ad
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-02-12 10:13:55 +0530

    About section updated in bottom nav bar

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import BestXP from './Best Experiences/BestXP'
+import DriveFree from './Best Experiences/DriveFree'
+import BtmNav from './Bottom  Nav/BtmNav'
 import Hero from './Hero'
 import Marketplace from './Marketplace Components/Marketplace'
 import RangeCars from './RangeCars'
+import Ulbenifits from './Unlimited Benifits/Ulbenifits'
 
 export default function Homepage() {
   return (
@@ -9,6 +13,10 @@ export default function Homepage() {
       <Hero/>
       <RangeCars/>
       <Marketplace/>
+      <Ulbenifits/>
+      <BestXP/>
+      <DriveFree/>
+      <BtmNav/>
     </div>
   )
 }
